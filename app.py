8f243d6168e2dfe713d889d348816e48fdf2f40e
Devangana T S <148966769+Thakkudts@users.noreply.github.com>
2026-09-17 09:27:59 +0530

    Implement prediction result feedback
    Add success and error messages based on prediction result.

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import joblib
+
+model=joblib.load("Logistic_regression_Student_StudyHrs_model.pkl")
+
+st.title("Student Pass/Fail based on Study Hours")
+
+hours=st.number_input("Enter Study Hours",min_value=0.0,max_value=15.0,value=5.0)
+
+if st.button("Predict"):
+  prediction=model.predict([[hours]])
+  if prediction[0]==1:
+    st.success("Pass")
+  else:
+    st.error("Fail")
