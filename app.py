be82770ae1dd0b228fa1eec1a23bec8d32d2c91a
Devangana T S <148966769+Thakkudts@users.noreply.github.com>
2026-09-17 11:11:14 +0530

    after adding attendance along with the study hours

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
 import streamlit as st
 import joblib
 
-model=joblib.load("Logistic_regression_Student_StudyHrs_model.pkl")
-st.title("Student Pass/Fail based on Study Hours")
-hours=st.number_input("Enter Study Hours:", min_value=0.0 ,max_value=15.0, value=5.0)
-if st.button("Predict"):
-  prediction=model.predict([[hours]])
-  probability = model.predict_proba([[hours]])
-  pass_prob=probability[0][1]*100
-  fail_prob=probability[0][0]*100
-  if prediction[0]==1:
-    st.success("Pass")
-    st.write("Probability of Pass:", round(pass_prob,2),"%")
+model = joblib.load("Logistic_regression_Student_StudyHrs_model.pkl")
 
-  else:
-    st.error("Fail")
-    st.write("Probability of Fail:", round(fail_prob,2),"%")
+st.title("Student Pass/Fail Prediction")
+
+hours = st.number_input("Enter Study Hours:",min_value=0.0,max_value=15.0,value=5.0)
+
+attendance = st.number_input("Enter Attendance:",min_value=0.0,max_value=100.0,value=75.0)
+
+if st.button("Predict"):
+
+    new_data = [[hours, attendance]]
+
+    prediction = model.predict(new_data)
+
+    probability = model.predict_proba(new_data)
+
+    pass_prob = probability[0][1] * 100
+    fail_prob = probability[0][0] * 100
+
+    if prediction[0] == 1:
+        st.success("Pass")
+        st.write("Probability of Pass:", round(pass_prob, 2), "%")
+        st.write("Probability of Fail:", round(fail_prob, 2), "%")
+
+    else:
+        st.error("Fail")
+        st.write("Probability of Fail:", round(fail_prob, 2), "%")
+        st.write("Probability of Pass:", round(pass_prob, 2), "%")
