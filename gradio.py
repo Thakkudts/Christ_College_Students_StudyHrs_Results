763c1d9b7bc2edc03140e8fa2f033881f269cd36
Devangana T S <148966769+Thakkudts@users.noreply.github.com>
2026-09-18 12:13:17 +0530

    Update model file name for loading

--- gradio.py
+++ gradio.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 
 # Load model
-model = joblib.load("Student_Std_Hrs.pkl")
+model = joblib.load("Logistic_regression_Student_StudyHrs_model (1).pkl")
 
 
 def predict_result(study_hours):
